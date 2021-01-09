8bd5da4d5a978ddd76a795ba4fe3368a72be34c5
ra-res <ra-res@users.noreply.github.com>
2021-01-09 13:19:02 +0000

    added output files

--- GameState.java
+++ GameState.java
@@ -6,146 +6,155 @@ public class GameState {
 
     private final int[][] board;
     public static final int SIZE = 3;
-    static final int[][] INITIAL_BOARD = {{8,7,6},{5,4,3},{2,1,0}};
-    static final int[][] GOAL_BOARD = {{1,2,3},{4,5,6},{7,8,0}};
+    static final int[][] INITIAL_BOARD = {{8, 7, 6}, {5, 4, 3}, {2, 1, 0}};
+    static final int[][] GOAL_BOARD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
     /**
      * GameState
      * Constructor for GameState, sets board to the int[][] passed as argument
+     *
      * @param board int matrix holding the 8 puzzle board configuration
      */
-    public GameState(int[][] board)
-    {
+    public GameState(int[][] board) {
         this.board = board;
     }
 
     /**
      * clone
+     *
      * @return a new GameState with the same board configuration as the current GameState.
      */
-    public GameState clone()
-    {
+    @Override
+    public GameState clone() {
         int[][] clonedBoard = new int[SIZE][SIZE];
-        for (int i = 0; i < this.board.length; i++)
-            System.arraycopy(this.board[i], 0, clonedBoard[i], 0, this.board[0].length);
+        for (int i = 0; i < board.length; i++) {
+            System.arraycopy(board[i], 0, clonedBoard[i], 0, board[0].length);
+        }
         return new GameState(clonedBoard);
     }
 
     /**
      * toString
+     *
      * @return toString returns the board configuration of the current GameState as a printable string.
      */
     @Override
-    public String toString()
-    {
-        String temp = Arrays.deepToString(this.board);
-        return temp;
+    public String toString() {
+        return Arrays.deepToString(board);
     }
 
     /**
      * isGoal
+     *
      * @return true if and only if the board configuration of the current GameState is the goal configuration.
      */
-    public boolean isGoal()
-    {
-        for (int i = 0; i < SIZE; i++)
-            for (int j = 0; j < SIZE; j++)
-                if (this.board[i][j] != GOAL_BOARD[i][j]) return false;
+    public boolean isGoal() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] != GOAL_BOARD[i][j]) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
     /**
      * isSolvable
      * reference: https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
+     *
      * @return true if number of inversions is even, false if number of inversions is odd.
      */
-    public boolean isSolvable()
-    {
+    public boolean isSolvable() {
         int inversions = 0;
-        for (int i = 0; i < SIZE - 1; i++)
-            for (int j = i + 1; j < SIZE; j++)
-                if (this.board[j][i] > 0 &&
-                        this.board[j][i] > this.board[i][j])
+        for (int i = 0; i < SIZE - 1; i++) {
+            for (int j = i + 1; j < SIZE; j++) {
+                if (board[j][i] > 0 &&
+                        board[j][i] > board[i][j]) {
                     inversions++;
-        return inversions%2==0;
+                }
+            }
+        }
+        return inversions % 2 != 0;
     }
 
     /**
      * sameBoard
+     *
      * @param gs - GameState to compare this.board to gs.board
      * @return true if and only if the GameState supplied as argument has the same board
      */
-    public boolean sameBoard(GameState gs)
-    {
-        for (int i = 0; i < SIZE; i++)
-            for (int j = 0; j < SIZE; j++)
-                if (this.board[i][j] != gs.board[i][j]) return false;
+    public boolean sameBoard(GameState gs) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] != gs.board[i][j]) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
     /**
      * getBoard
+     *
      * @return the board corresponding to the object
      */
-    public int[][] getBoard()
-    {
-        return this.board;
+    public int[][] getBoard() {
+        return board;
     }
 
     /**
      * swap
+     *
      * @param i - index corresponding to row
      * @param j - index corresponding to col
      * @param x - index corresponding to row
      * @param y - index corresponding to col
      */
-    public void swap(int i, int j, int x, int y)
-    {
-        int temp = this.board[i][j];
-        this.board[i][j] = this.board[x][y];
-        this.board[x][y] = temp;
+    public void swap(int i, int j, int x, int y) {
+        int temp = board[i][j];
+        board[i][j] = board[x][y];
+        board[x][y] = temp;
     }
 
     /**
      * calculateHeuristic
+     *
      * @return estimated distance to goal state
      */
-    public int calculateHeuristic()
-    {
+    public int calculateHeuristic() {
         int distance = 0;
-        for (int i = 0; i < SIZE; i++)
-            for (int j = 0; j < SIZE; j++)
-                if (board[i][j] != 0 && board[i][j] != GOAL_BOARD[i][j])
-                {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] != 0 && board[i][j] != GOAL_BOARD[i][j]) {
                     int division = (board[i][j] - 1) / SIZE;
                     int modulo = (board[i][j] - 1) % SIZE;
-                    distance += Math.abs(division-i) + Math.abs(modulo-j);
+                    distance += Math.abs(division - i) + Math.abs(modulo - j);
                 }
+            }
+        }
         return distance;
     }
 
     /**
      * possibleMoves
+     *
      * @return list of all GameStates that can be reached in a single move from the current GameState.
      */
-    public ArrayList<GameState> possibleMoves()
-    {
+    public ArrayList<GameState> possibleMoves() {
         ArrayList<GameState> moves = new ArrayList<GameState>();
         HashMap<String, Integer[]> potentialMoves = new HashMap<>();
 
-        for (int i = 0; i < SIZE; i++)
-        {
-            for( int j = 0; j < SIZE; j++)
-            {
-                potentialMoves.put("WEST",new Integer[]{i, j-1});
-                potentialMoves.put("EAST",new Integer[]{i, j+1});
-                potentialMoves.put("SOUTH",new Integer[]{i+1, j});
-                potentialMoves.put("NORTH",new Integer[]{i-1, j});
-                for (Integer[] dir  : potentialMoves.values())
-                {
-                    if (dir[0] >= 0 && dir[1] >= 0 && dir[0] < SIZE && dir[1] < SIZE && board[dir[0]][dir[1]] == 0)
-                    {
-                        GameState newState = this.clone();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                potentialMoves.put("WEST", new Integer[]{i, j - 1});
+                potentialMoves.put("EAST", new Integer[]{i, j + 1});
+                potentialMoves.put("SOUTH", new Integer[]{i + 1, j});
+                potentialMoves.put("NORTH", new Integer[]{i - 1, j});
+                for (Integer[] dir : potentialMoves.values()) {
+                    if (dir[0] >= 0 && dir[1] >= 0 && dir[0] < SIZE && dir[1] < SIZE && board[dir[0]][dir[1]] == 0) {
+                        GameState newState = clone();
                         newState.swap(i, j, dir[0], dir[1]);
                         moves.add(newState);
                     }
--- Solver.java
+++ Solver.java
@@ -1,68 +1,69 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
-   Solver is a class that contains the methods used to search for and print solutions
-   plus the data structures needed for the search.
+ * Solver is a class that contains the methods used to search for and print solutions
+ * plus the data structures needed for the search.
  */
 
 public class Solver {
 
     ArrayList<Node> unexpanded = new ArrayList<>(); // Holds unexpanded node list
-    ArrayList<Node> expanded  = new ArrayList<>();  // Holds expanded node list
+    ArrayList<Node> expanded = new ArrayList<>();  // Holds expanded node list
     Node rootNode;                                      // Node representing initial state
 
     /**
      * Solver
      * Solver is a constructor that sets up an instance of the class
      * with a node corresponding to the initial state as the root node.
+     *
      * @param initialBoard
      */
-    public Solver(int[][] initialBoard)
-    {
+    public Solver(int[][] initialBoard) {
         GameState initialState = new GameState(initialBoard);
         rootNode = new Node(initialState);
     }
 
     /**
      * getLowest
+     *
      * @return Node with lowest cost value
      * For uniform cost search, heuristic will be initialised to 0 to promote versatility
      * so adding it won't change the outcome.
      * Method can be used for both uniform cost search and a* search
      */
-    public Node getLowest()
-    {
+    public Node getLowest() {
         Node n = unexpanded.get(0);
-        for (Node node : unexpanded)
-            if ((node.getHeuristic()+node.getCost()) < (n.getCost() + n.getHeuristic()))
-                n=node;
+        for (Node node : unexpanded) {
+            if ((node.getHeuristic() + node.getCost()) < (n.getCost() + n.getHeuristic())) {
+                n = node;
+            }
+        }
         return n;
     }
 
     /**
      * UniformCostSolve
      * Uniform cost search algorithm implementation to solve the 8 puzzle
+     *
      * @param output - where output should be directed
      */
-    public void UniformCostSolve(PrintWriter output)
-    {
-
-        if (!rootNode.state.isSolvable()) // checks to see if gamestate is not solvable
+    public void UniformCostSolve(PrintWriter output) {
+        if (rootNode.state.isSolvable()) // checks to see if gamestate is not solvable
         {
             output.println("No solution found");
             return;
         }
 
         unexpanded.add(rootNode); // adds node to unexpanded
-        while (!unexpanded.isEmpty())
-        {
+        while (!unexpanded.isEmpty()) {
             Node n = getLowest(); // Lowest cost node
             unexpanded.remove(n); // Removes lowest cost node from unexpanded
-            if (n.state.isGoal()) // Checks to see if node is goal
-            {
-                reportSolution(n,output);
+            if (n.state.isGoal()) { // Checks to see if node is goal{
+                reportSolution(n, output);
                 return;
             }
             expanded.add(n); // Adds node to expanded
@@ -71,7 +72,7 @@ public class Solver {
             {
                 if ((Node.findNodeWithState(unexpanded, gs) == null) && (Node.findNodeWithState(expanded, gs) == null)) //Check to see if state already been visited
                 {
-                    Node newNode = new Node(gs, n, n.getCost() + 1,0); // Generates new node, increments cost++
+                    Node newNode = new Node(gs, n, n.getCost() + 1, 0); // Generates new node, increments cost++
                     unexpanded.add(newNode); // Adds new node to unexpanded
                 }
             }
@@ -82,24 +83,22 @@ public class Solver {
     /**
      * AStarSolve
      * A* algorithm implementation to solve the 8 puzzle
+     *
      * @param output - where output should be directed
      */
-    public void AStarSolve(PrintWriter output)
-    {
-        if (!rootNode.state.isSolvable()) // checks to see if gamestate is not solvable
+    public void AStarSolve(PrintWriter output) {
+        if (rootNode.state.isSolvable()) // checks to see if gamestate is not solvable
         {
             output.println("No solution found");
             return;
         }
         int heuristic;
         unexpanded.add(rootNode); // adds node to unexpanded
-        while (!unexpanded.isEmpty())
-        {
+        while (!unexpanded.isEmpty()) {
             Node n = getLowest(); // Gets lowest cost node
             unexpanded.remove(n); // Removes lowest cost node from unexpanded
-            if (n.state.isGoal())
-            {
-                reportSolution(n,output);
+            if (n.state.isGoal()) {
+                reportSolution(n, output);
                 return;
             }
             expanded.add(n); // Adds node to expanded
@@ -121,33 +120,35 @@ public class Solver {
      * printSolution is a recursive method that prints all the states in a solution.
      * It uses the parent links to trace from the goal to the initial state then prints
      * each state as the recursion unwinds.
-     * @param n node representing the goal state.
+     *
+     * @param n      node representing the goal state.
      * @param output - where the output should be directed.
      */
-    public void printSolution(Node n, PrintWriter output)
-    {
-        if (n.parent != null) printSolution(n.parent, output);
+    public void printSolution(@NotNull Node n, PrintWriter output) {
+        if (n.parent != null) {
+            printSolution(n.parent, output);
+        }
         output.println(n.state);
     }
 
     /**
      * reportSolution prints the solution together with statistics on the number of moves
      * and the number of expanded and unexpanded nodes.
-     * @param n - node representing the goal state.
+     *
+     * @param n      - node representing the goal state.
      * @param output - where the output should be directed.
      */
-    public void reportSolution(Node n, PrintWriter output)
-    {
+    public void reportSolution(Node n, PrintWriter output) {
         output.println("Solution found!");
         printSolution(n, output);
         output.println(n.getCost() + " Moves");
-        output.println("Nodes expanded: " + this.expanded.size());
-        output.println("Nodes unexpanded: " + this.unexpanded.size());
+        output.println("Nodes expanded: " + expanded.size());
+        output.println("Nodes unexpanded: " + unexpanded.size());
         output.println();
     }
 
-    public static void main(String[] args) throws Exception
-    {
+
+    public static void main(String[] args) throws Exception {
 
         long startTime = System.nanoTime();
         Solver problem = new Solver(GameState.INITIAL_BOARD);  // Set up the problem to be solved.
@@ -156,7 +157,7 @@ public class Solver {
         problem.UniformCostSolve(output);                      // Search for and print the solution
         output.close();                                        // Close the PrintWriter (to ensure output is produced).
         long endTime = System.nanoTime();
-        long duration = (endTime - startTime)/1000000;
+        long duration = (endTime - startTime) / 1000000;
         System.out.println(duration);
 
 
@@ -167,7 +168,7 @@ public class Solver {
         problem2.AStarSolve(output2);                           // Search for and print the solution
         output2.close();                                        // Close the PrintWriter (to ensure output is produced).
         endTime = System.nanoTime();
-        duration = (endTime - startTime)/1000000;
+        duration = (endTime - startTime) / 1000000;
         System.out.println(duration);
 
     }
